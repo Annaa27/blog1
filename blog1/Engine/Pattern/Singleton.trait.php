3f0d28dbcd0fbefedacdb5944eeb8554f9828621
user <user@kab305-11>
2019-03-20 19:58:40 +0200

    20.03.19

---
+++ blog1/Engine/Pattern/Singleton.trait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TestProject\Engine\Pattern;
+
+trait Singleton
+{
+    use Base;
+    
+    protected static $_oInstance = null;
+    
+    public static function getInstance()
+    {
+        return (null === static::$_oInstance) ? static::$_oInstance = new static : static::$_oInstance;
+        
+    }
+}
